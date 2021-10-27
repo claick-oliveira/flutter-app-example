75bf7b4c6ba03d572b477459b4dd0f099e8b8046
claick-oliveira <claick.cao@gmail.com>
2021-10-26 23:52:46 -0300

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: TransfersList(),
         appBar: AppBar(
           title: const Text('Transfers'),
         ),
@@ -12,3 +13,45 @@ void main() => runApp(MaterialApp(
       ),
       debugShowCheckedModeBanner: true,
     ));
+
+// ignore: use_key_in_widget_constructors
+class TransfersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransferItem(Transfer(100.0, 1000)),
+        TransferItem(Transfer(100.1, 1000)),
+        TransferItem(Transfer(100.2, 1000)),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  // ignore: prefer_const_constructors_in_immutables, use_key_in_widget_constructors
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      // ignore: prefer_const_constructors
+      leading: Icon(
+        Icons.monetization_on,
+        size: 62.0,
+      ),
+      title: Text(_transfer.value.toString()),
+      subtitle: Text(_transfer.accountNumber.toString()),
+    ));
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
+}

8f6f3e3daf9c8eaebca2b9e27ee9a4d44298cf12
claick-oliveira <claick.cao@gmail.com>
2021-10-27 00:36:58 -0300

    Added New Transfer Form

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,83 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(const BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+  static const String _title = 'Bytebank';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
       home: Scaffold(
-        body: TransfersList(),
-        appBar: AppBar(
-          title: const Text('Transfers'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ),
+        body: NewTransferForm(),
       ),
       debugShowCheckedModeBanner: true,
-    ));
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class NewTransferForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('New Transfer'),
+        ),
+        body: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: 'Account Number', hintText: '0000'),
+                style: TextStyle(fontSize: 24.0),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.monetization_on,
+                      size: 50.0,
+                    ),
+                    labelText: 'Value',
+                    hintText: '0.00'),
+                style: TextStyle(fontSize: 24.0),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Send'),
+            )
+          ],
+        ));
+  }
+}
 
 // ignore: use_key_in_widget_constructors
 class TransfersList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TransferItem(Transfer(100.0, 1000)),
-        TransferItem(Transfer(100.1, 1000)),
-        TransferItem(Transfer(100.2, 1000)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transfers'),
+      ),
+      body: Column(
+        children: [
+          TransferItem(Transfer(100.0, 1000)),
+          TransferItem(Transfer(100.1, 1000)),
+          TransferItem(Transfer(100.2, 1000)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }

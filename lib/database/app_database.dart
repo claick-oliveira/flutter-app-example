9fb2579abffc5972c6e3ae2f8383b8caaea625c5
claick-oliveira <claick.cao@gmail.com>
2021-11-03 16:52:42 -0300

    Refactor database connections

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -3,90 +3,83 @@ import 'package:bytebank/models/transaction.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
-    return openDatabase(
-      path,
-      onCreate: (db, version) {
-        db.execute(
-          'CREATE TABLE contacts('
-          'id INTEGER PRIMARY KEY AUTOINCREMENT, '
-          'name TEXT, '
-          'account_number INTEGER)',
-        );
-        db.execute(
-          'CREATE TABLE transactions('
-          'id INTEGER PRIMARY KEY AUTOINCREMENT, '
-          'value REAL, '
-          'account_number INTEGER, '
-          'time TEXT)',
-        );
-      },
-      version: 1,
-      onDowngrade: onDatabaseDowngradeDelete,
+Future<Database> getDatabase() async {
+  final String dbPath = await getDatabasesPath();
+  final String path = join(dbPath, 'bytebank.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute(
+        'CREATE TABLE contacts('
+        'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+        'name TEXT, '
+        'account_number INTEGER)',
+      );
+      db.execute(
+        'CREATE TABLE transactions('
+        'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+        'value REAL, '
+        'account_number INTEGER, '
+        'time TEXT)',
+      );
+    },
+    version: 1,
+    //onDowngrade: onDatabaseDowngradeDelete,
+  );
+}
+
+Future<int> saveContact(ContactModel contact) async {
+  final Database db = await getDatabase();
+  final Map<String, dynamic> contactMap = {};
+  contactMap['name'] = contact.name;
+  contactMap['account_number'] = contact.accountNumber;
+  return db.insert(
+    'contacts',
+    contactMap,
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
+}
+
+Future<int> saveTransaction(TransactionModel transaction) async {
+  final Database db = await getDatabase();
+  final Map<String, dynamic> transactionMap = {};
+  transactionMap['value'] = transaction.value;
+  transactionMap['account_number'] = transaction.accountNumber;
+  transactionMap['time'] = transaction.time;
+  return db.insert(
+    'transactions',
+    transactionMap,
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
+}
+
+Future<List<ContactModel>> findAllContacts() async {
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> result = await db.query('contacts');
+  final List<ContactModel> contacts = [];
+  for (Map<String, dynamic> row in result) {
+    final ContactModel contact = ContactModel(
+      row['id'],
+      row['name'],
+      row['account_number'],
     );
-  });
+    contacts.add(contact);
+  }
+  return contacts;
 }
 
-Future<int> saveContact(ContactModel contact) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> contactMap = {};
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.accountNumber;
-    return db.insert(
-      'contacts',
-      contactMap,
-      conflictAlgorithm: ConflictAlgorithm.replace,
+Future<List<TransactionModel>> findAllTransactions() async {
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> result = await db.query('transactions');
+  final List<TransactionModel> transactions = [];
+  for (Map<String, dynamic> row in result) {
+    final TransactionModel transaction = TransactionModel(
+      row['id'],
+      row['value'],
+      row['account_number'],
+      row['time'],
     );
-  });
-}
-
-Future<int> saveTransaction(TransactionModel transfer) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> transferMap = {};
-    transferMap['value'] = transfer.value;
-    transferMap['account_number'] = transfer.accountNumber;
-    transferMap['time'] = transfer.time;
-    return db.insert(
-      'transactions',
-      transferMap,
-      conflictAlgorithm: ConflictAlgorithm.replace,
-    );
-  });
-}
-
-Future<List<ContactModel>> findAllContacts() {
-  return createDatabase().then((db) {
-    return db.query('contacts').then((maps) {
-      final List<ContactModel> contacts = [];
-      for (Map<String, dynamic> map in maps) {
-        final ContactModel contact = ContactModel(
-          map['id'],
-          map['name'],
-          map['account_number'],
-        );
-        contacts.add(contact);
-      }
-      return contacts;
-    });
-  });
-}
-
-Future<List<TransactionModel>> findAllTransactions() {
-  return createDatabase().then((db) {
-    return db.query('transactions').then((maps) {
-      final List<TransactionModel> transactions = [];
-      for (Map<String, dynamic> map in maps) {
-        final TransactionModel transaction = TransactionModel(
-          map['id'],
-          map['value'],
-          map['account_number'],
-          map['time'],
-        );
-        transactions.add(transaction);
-      }
-      return transactions;
-    });
-  });
+    transactions.add(transaction);
+  }
+  return transactions;
 }
